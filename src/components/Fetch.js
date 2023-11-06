bf3d64d31333b5d223b9a5da26c0f7d15869bc59
Michely <michelyparedes@gmail.com>
2023-11-06 10:57:49 +0100

    final fix to Fetch function

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,75 +1,81 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
+function Fetch() {
+  const [accessToken, setAccessToken] = useState(null);
 
-function Fetch () {
-    const [accessToken, setAccessToken] = useState(null);
+  const localStorage = window.localStorage;
 
-    const localStorage = window.localStorage;
+  useEffect(() => {
+    async function getToken() {
+      const CLIENT_ID = "9a68f87255b544308a6fbebbd710b53f";
+      const CLIENT_SECRET = "da3cea8f10f74e99a6d514abe260dfb3";
+      const URL = "https://accounts.spotify.com/api/token";
 
-    useEffect(() => {
-        async function getToken(){
-            const CLIENT_ID = '9a68f87255b544308a6fbebbd710b53f';
-            const CLIENT_SECRET = 'da3cea8f10f74e99a6d514abe260dfb3';
-            const URL = 'https://accounts.spotify.com/api/token';
+      let body = new URLSearchParams();
+      body.append("grant_type", "client_credentials");
+      body.append("client_id", CLIENT_ID);
+      body.append("client_secret", CLIENT_SECRET);
 
-            const options={
-                method:'POST',
-                headers:{
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body:'grant_type=client_credetions&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-            };
-
-            try{
-                const request = await fetch (URL, options);
-                const response = await request.json();
-
-                if(request.ok){
-                    setAccessToken(response.access_token);
-                    localStorage.setItem('access_token', response.access_token);
-                    /* console.log(response); to check API results? */
-                }
-            } catch(error){
-                console.log(error);
-            } 
-        }
-
-        getToken();
-
-    },
-    []
-    );
-
-    // Search Function
-
-    const [albums, setAlbums] = useState([]);
-    
-    const URL='https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks';
-
-        const options = {
-        method: 'GET',
+      const options = {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + accessToken,
-        }
-        };
-    
-    fetch(URL, options)
-        .then(response => response.json())
-        .then(result => {
-        setAlbums(albums);
-        console.log(result)
-        })
-        .catch(err => console.error(err))
-    
-    return(
-        <div>{
-            albums.map(album =>
-            <img src={album.images} className="" alt="image" />
-            )}
-        </div>
-    )
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: body,
+      };
 
+      try {
+        const request = await fetch(URL, options);
+        const response = await request.json();
+
+        if (request.ok) {
+          setAccessToken(response.access_token);
+          localStorage.setItem("access_token", response.access_token);
+          setDownloadAlbums(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getToken();
+  }, []);
+
+  // Search Function
+
+  const [albums, setAlbums] = useState([]);
+  const [downloadAlbums, setDownloadAlbums] = useState(false);
+
+  const URL =
+    "https://api.spotify.com/v1/browse/new-releases?country=ES&limit=15";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+  };
+
+  useEffect(() => {
+    if (downloadAlbums) {
+      fetch(URL, options)
+        .then((response) => response.json())
+        .then((result) => {
+          setAlbums(result.albums.items);
+          console.log(result);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [downloadAlbums]);
+
+  return (
+    <div>
+      {albums.map((album) => (
+        <img src={album.images[0].url} className="" alt="image" />
+      ))}
+    </div>
+  );
 }
 
 export default Fetch;
