24bff71dd1296f972f102c46adeb4f4b8aa0eb75
ccm24 <113595661+ccm24@users.noreply.github.com>
2023-11-08 10:26:46 +0100

    creation .env to hide tokens

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <div>
-      <img class="logoAlbumPage" src={completeLogo} />
+      <img className="logoAlbumPage" src={completeLogo} />
     </div>
   );
 }
